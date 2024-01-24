2bfae0a6d1f23a4a67e068c931dab1cc193528d4
Erik Gomes <erikgtorres28@gmail.com>
2024-01-23 22:58:56 -0300

    first commit

---
+++ scripts.js
@@ -0,0 +1,22 @@
+const drawButton = document.getElementById("draw-button")
+
+function draw() {
+    const initialNumberInput = document.getElementById("initial-number").value
+    const limitNumberInput = document.getElementById("limit-number").value
+    const drawnNumberParagraph = document.getElementById("drawn-number-paragraph")
+
+    const max = Math.ceil(limitNumberInput)
+    const min = Math.floor(initialNumberInput)
+
+    if (initialNumberInput.length === 0 || limitNumberInput.length === 0) {
+        alert("Preencha todos os campos!")
+    } else if (initialNumberInput >= limitNumberInput) {
+        alert("O número inicial deve ser menor que o número limite!")
+    } else {
+        const drawnNumber = Math.floor(Math.random() * (max - min + 1)) + min
+
+        drawnNumberParagraph.innerHTML = drawnNumber
+    }
+}
+
+drawButton.addEventListener("click", draw)
